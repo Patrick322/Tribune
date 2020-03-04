8fe921980f5409302dd29744150dc290af4c939b
Patrick595 <patrickbuong@gmail.com>
2020-03-04 12:39:49 +0300

    modified ied the application

--- news/models.py
+++ news/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 import datetime as dt
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
 # Create your models here.
 class Editor(models.Model):
@@ -24,12 +26,12 @@ class tags(models.Model):
         return self.name
 
 class Article(models.Model):
-    title = models.CharField(max_length =60)
-    post = models.TextField()
-    editor = models.ForeignKey(Editor)
+    title = models.CharField(max_length=60)
+    post = HTMLField()
+    editor = models.ForeignKey(User,on_delete=models.CASCADE) 
     tags = models.ManyToManyField(tags)
     pub_date = models.DateTimeField(auto_now_add=True)
-    article_image = models.ImageField(upload_to = 'articles/' ,blank=True)
+    article_image = models.ImageField(upload_to='articles/', blank=True)
 
     def __str__(self):
         return self.title
@@ -59,4 +61,8 @@ class Article(models.Model):
     @classmethod
     def days_news(cls,date):
         news = cls.objects.filter(pub_date__date = date)
-        return news    
+        return news
+
+class NewsLetterRecipients(models.Model):
+    name = models.CharField(max_length = 30)
+    email = models.EmailField()        
